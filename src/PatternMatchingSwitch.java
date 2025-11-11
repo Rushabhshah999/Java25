4c3ad85bfd3a5565777e6dc46ef6ff33b75e2cc8
Rushabh Shah <shah.rushabh.shah@gmail.com>
2025-11-11 15:18:56 +0100

    update for comments

--- src/PatternMatchingSwitch.java
+++ src/PatternMatchingSwitch.java
@@ -5,8 +5,12 @@
         Object obj = 100;
 
         switch (obj) {
-            case Integer i -> System.out.println("It's an int: " + i);
+            // && i > 5 preview
+            case Integer i  -> System.out.println("It's an int: " + i);
             default -> System.out.println("Something else: " + obj);
         }
+
+
+
     }
 
