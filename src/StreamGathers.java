399a19df4aac82cad316627ac62da94853005ffa
Rushabh Shah <shah.rushabh.shah@gmail.com>
2026-01-11 14:06:08 +0100

    ädded for stream gathers

--- src/StreamGathers.java
+++ src/StreamGathers.java
@@ -12,8 +12,13 @@ public class StreamGathers {
         List<List<Integer>> inpiutList = Stream.of(1,2,3,4,5,6,7)
                 .gather(Gatherers.windowFixed(2)).toList();
 
-        System.out.println(inpiutList);
+        System.out.println(inpiutList); // [[1, 2], [3, 4], [5, 6], [7]]
 
+
+        List<List<Integer>> inpiutList1 = Stream.of(1,2,3,4,5,6,7)
+                .gather(Gatherers.windowSliding(3)).toList();
+
+        System.out.println(inpiutList1); // [[1, 2, 3], [2, 3, 4], [3, 4, 5], [4, 5, 6], [5, 6, 7]]
 
         // before
         int windowSize = 2;
@@ -23,7 +28,7 @@ public class StreamGathers {
             List<List<Integer>> intList =  inpiutList.subList(i, Math.min(i + windowSize, inpiutList.size()));
             windows.add(intList);
         }
-        System.out.println(windows);
+        System.out.println(windows); //[[[1, 2], [3, 4]], [[5, 6], [7]]]
 
 
 
